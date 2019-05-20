d9dec820a2a1e5f77b4c07ac1f24794ecc230f7c
root <root@gpu01-extended.jojun5tu5u3utelnwbh5ao1ylh.bx.internal.cloudapp.net>
2019-05-20 10:34:53 +0000

    Added download script

---
+++ relative_cambridge/download.sh
@@ -0,0 +1,9 @@
+wget https://www.repository.cam.ac.uk/bitstream/handle/1810/251342/KingsCollege.zip
+wget https://www.repository.cam.ac.uk/bitstream/handle/1810/251340/OldHospital.zip
+wget https://www.repository.cam.ac.uk/bitstream/handle/1810/251336/ShopFacade.zip
+wget https://www.repository.cam.ac.uk/bitstream/handle/1810/251294/StMarysChurch.zip
+unzip KingsCollege.zip
+unzip OldHospital.zip
+unzip ShopFacade.zip
+unzip StMarysChurch.zip
+
