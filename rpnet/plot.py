8391a75b6a9a3e1cb6e960dc21f4081118f0c3e9
Ganesh Anand <gan3sh500@gmail.com>
2019-05-23 16:05:16 +0530

    Update plot.py

--- rpnet/plot.py
+++ rpnet/plot.py
@@ -30,6 +30,4 @@ def plot_to_image(figure):
     plt.savefig(buf, format='png')
     plt.close(figure)
     buf.seek(0)
-    image = tf.image.decode_png(buf.getvalue(), channels=4)
-    image = tf.expand_dims(image, 0)
-    return image
+    return buf.getvalue()
